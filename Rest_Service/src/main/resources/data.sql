8514690b2a905dedb00dd9480077e94b583890c6
Saagar <Saagar.Ahluwalia@bellevuecollege.edu>
2022-10-25 15:09:41 -0700

    Fixed SQL syntax error

--- Rest_Service/src/main/resources/data.sql
+++ Rest_Service/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into user values(1231, now() , 'jennifer');
-insert into user values(1235, now() , 'jake');
-insert into user values(1236, now() , 'allan');
-insert into user values(1237, now() , 'tim');
-insert into user values(1238, now() , 'harley');
-insert into user values(1239, now() , 'lue');
+insert into person values(1231, now() , 'jennifer');
+insert into person values(1235, now() , 'jake');
+insert into person values(1236, now() , 'allan');
+insert into person values(1237, now() , 'tim');
+insert into person values(1238, now() , 'harley');
+insert into person values(1239, now() , 'lue');
 
 insert into Post values(10001, 'my first post', 1231);
 insert into Post values(10002, 'my second post', 1231);
