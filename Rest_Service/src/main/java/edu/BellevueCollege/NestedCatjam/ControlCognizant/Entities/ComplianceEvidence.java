c07a54625b8cdd4b8391e321a5110b28dd78186e
Saagar <Saagar.Ahluwalia@bellevuecollege.edu>
2022-11-06 21:39:27 -0800

    Add entity for evidence of compliance and enums for frameworks and user role

---
+++ Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Entities/ComplianceEvidence.java
@@ -0,0 +1,22 @@
+package edu.BellevueCollege.NestedCatjam.ControlCognizant.Entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Blob;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "compliance_evidence")
+public class ComplianceEvidence implements ControlCognizantEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Lob
+    private Blob evidence;
+    @ManyToMany
+    private List<Control> controlsImplemented;
+}
