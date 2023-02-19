3b060a92fc1ee94f4ef062517d5ad7534cbae667
Saagar <Saagar.Ahluwalia@bellevuecollege.edu>
2023-02-18 19:02:21 -0800

    Change to merge both git branches in a way that builds

--- Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Controllers/UserController.java
+++ Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Controllers/UserController.java
@@ -1,8 +1,10 @@
 package edu.BellevueCollege.NestedCatjam.ControlCognizant.Controllers;
 
-import edu.BellevueCollege.NestedCatjam.ControlCognizant.Entities.User;
+import com.auth0.exception.Auth0Exception;
+import com.auth0.json.mgmt.users.User;
 import edu.BellevueCollege.NestedCatjam.ControlCognizant.Exceptions.UserNotFoundException;
 import edu.BellevueCollege.NestedCatjam.ControlCognizant.Repositories.UserRepository;
+import edu.BellevueCollege.NestedCatjam.ControlCognizant.services.UserManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
@@ -10,16 +12,19 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepository repository;
 
+    @Autowired private UserManagementService userManagementService;
+
     @GetMapping("/api/users/get-all-users")
-    public List<User> retrieveAllUsers() {
+    public List<User> retrieveAllUsers() throws Auth0Exception {
         try {
-            return repository.findAll();
+            return userManagementService.getUsers();
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
@@ -27,33 +32,23 @@ public class UserController {
         }
     }
 
-    @GetMapping("/users/{id}")
-    public Optional<User> getUserById(@PathVariable UUID id) {
+
+
+    @PostMapping("/api/users/post-user")
+    public String postUser(@Valid @RequestBody com.auth0.json.mgmt.users.User user) throws Auth0Exception {
         try {
-            return repository.findById(id).or(() -> {
-                throw new UserNotFoundException("user with id " + id + " not found");
-            });
+            userManagementService.addUser(user);
+            return "saved user with id: " + user.getId();
         } catch (Exception e) {
             e.printStackTrace();
+            return "Failed to save user with the following error: " + e.getMessage();
         }
-        return Optional.empty();
-    }
-
-    @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable String id) throws Auth0Exception {
-        System.out.println(id);
-        userManagementService.delete(id);
-    }
-    @PostMapping("/users")
-    public com.auth0.json.mgmt.users.User postUser(@Valid @RequestBody com.auth0.json.mgmt.users.User user) throws Auth0Exception {
-        System.out.println(user.getEmail().toString());
-        return userManagementService.addUser(user);
     }
 
     @GetMapping("/api/users/user/{id}")
     public String getUserById(@PathVariable long id) {
         try {
-            for (User user : repository.findAll()) {
+            for (final var user : repository.findAll()) {
                 if (user.getId() == id) {
                     return "user with id " + id + " found: " + user.toString();
                 }
@@ -65,57 +60,23 @@ public class UserController {
         return null;
     }
 
-    @PostMapping(value = "/users/{id}/roles", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/api/users/user/{id}/roles", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void assignRole(@PathVariable String id, @RequestBody List<String> role) throws Auth0Exception {
         userManagementService.assignRole(id, role);
     }
 
     @DeleteMapping("/api/users/user/{id}")
-    public void deleteUser(@PathVariable long id) throws UserNotFoundException {
+    public void deleteUser(@PathVariable String id) throws Auth0Exception {
         try {
-            for (User user : repository.findAll()) {
-                if (user.getId() == id) {
-                    repository.deleteById(id);
-                    return;
-                }
-            }
-            throw new UserNotFoundException("user with id " + id + " not found");
+            userManagementService.delete(id);
         } catch (Exception e) {
             e.printStackTrace();
+            throw e;
         }
     }
-    @PostMapping("/api/users/post-user")
-    public String postUser(@Valid @RequestBody User user) {
-        try {
-            repository.save(user);
-            return "saved user with id: " + user.getId();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "failed to save user";
-    }
+
     @PutMapping("/api/users/user/{id}")
-    public String updateUser(@RequestBody User user) {
-        for(User u : repository.findAll()) {
-            String tempID = String.valueOf(user.getId());
-            if(u.getId() == u.getId()) {
-                u.setEmail(user.getEmail());
-                u.setUserName(user.getUserName());
-                u.setId(user.getId());
-                u.setEnabled(user.isEnabled());
-                u.setPassword(user.getPassword());
-                u.setEmail(user.getEmail());
-                u.setLast_name(user.getLast_name());
-                u.setFirst_name(user.getFirst_name());
-                u.setChatLogs(user.getChatLogs());
-                u.setLocked(user.isLocked());
-                u.setRole(user.getRole());
-                u.setCredentialsExpired(user.isCredentialsExpired());
-                return "updated user with id: " + u.getId() + " to " + String.valueOf(user.getId());
-            } else {
-                return "failed to update user with id: " + u.getId();
-            }
-        }
-        return "failed to update user";
+    public void updateUser(@PathVariable String id, @RequestBody User user) throws Auth0Exception {
+        userManagementService.update(id, user);
     }
 }
