5342bf9dbccd10e51ecb492c74dd9bd823ce42e5
Chutton7743 <codeyhutton12@gmail.com>
2023-04-14 11:31:46 -0700

    Reworked control API and control entity to better match tables and python script

--- Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Controllers/ControlController.java
+++ Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Controllers/ControlController.java
@@ -4,31 +4,35 @@ import edu.BellevueCollege.NestedCatjam.ControlCognizant.Entities.Control;
 import edu.BellevueCollege.NestedCatjam.ControlCognizant.Exceptions.ControlNotFoundException;
 import edu.BellevueCollege.NestedCatjam.ControlCognizant.Repositories.ControlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/controls")
+@RequestMapping("/api/v1/controls")
 public class ControlController {
     @Autowired
     public ControlRepository controlRepository;
 
     @GetMapping("/{id}")
-    public Control getControl(@PathVariable long id) {
+    public ResponseEntity<Object> getControl(@PathVariable long id) {
         try {
-            return controlRepository.findById(id).orElseThrow(() -> new ControlNotFoundException("id = " + id));
+            Control control = controlRepository.findById(id).orElseThrow(() -> new ControlNotFoundException("id = " + id));
+            return new ResponseEntity<>(control, HttpStatus.OK);
         } catch (ControlNotFoundException e) {
             e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
-        return null;
     }
 
     @PostMapping
-    public Control createControl(@RequestBody Control control) {
+    public ResponseEntity<Control> createControl(@RequestBody Control control) {
         try {
-            return controlRepository.save(control);
+            Control savedControl = controlRepository.save(control);
+            return new ResponseEntity<>(savedControl, HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return null;
     }
 }
--- Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Entities/Control.java
+++ Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Entities/Control.java
@@ -1,11 +1,8 @@
 package edu.BellevueCollege.NestedCatjam.ControlCognizant.Entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-
 import javax.persistence.*;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Entity
 @Data
@@ -14,27 +11,40 @@ public class Control {
     @Id
     @GeneratedValue(strategy = javax.persistence.GenerationType.SEQUENCE)
     @Column(name = "control_id")
-    private long id;
+    private long control_id;
 
     @Column(name = "control_function")
-    private String controlFunction;
+    private String control_function;
 
     @Column(name = "control_category")
-    private String controlCategory;
+    private String control_category;
 
     @Column(name = "control_subcategory_description")
-    private String controlCategoryDescription;
+    private String control_subcategory_description;
 
     @Column(name = "control_subcategory_id")
-    private String controlSubCategoryId;
+    private String control_subcategory_id;
 
-    @Column(name = "Control_Name")
+    @Column(name = "control_name")
     @Size(min = 2, message= "Name must be longer than 2 characters")
-    private String name;
+    private String control_name;
 
     @Column(name = "control_description")
-    private String description;
+    private String control_description;
 
     @Column(name= "control_mapping_id")
-    private long mapping;
+    private long control_mapping_id;
+
+    public Control() {
+    }
+
+    public Control(String control_function, String control_category, String control_subcategory_description,
+                   String control_subcategory_id, String control_name, String control_description) {
+        this.control_function = control_function;
+        this.control_category = control_category;
+        this.control_subcategory_description = control_subcategory_description;
+        this.control_subcategory_id = control_subcategory_id;
+        this.control_name = control_name;
+        this.control_description = control_description;
+    }
 }
