0b81722ca7746fd1f5ebfc0a24a43656a99b6735
Saagar <Saagar.Ahluwalia@bellevuecollege.edu>
2023-04-17 12:13:20 -0700

    Begin using organizations

--- Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/services/UserManagementService.java
+++ Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/services/UserManagementService.java
@@ -3,15 +3,22 @@ package edu.BellevueCollege.NestedCatjam.ControlCognizant.services;
 import com.auth0.client.HttpOptions;
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.client.mgmt.ManagementAPI;
+import com.auth0.client.mgmt.filter.PageFilter;
 import com.auth0.client.mgmt.filter.UserFilter;
 import com.auth0.exception.Auth0Exception;
 import com.auth0.json.auth.TokenHolder;
 import com.auth0.json.mgmt.Role;
+import com.auth0.json.mgmt.organizations.Member;
+import com.auth0.json.mgmt.organizations.Members;
+import com.auth0.json.mgmt.organizations.Organization;
 import com.auth0.json.mgmt.users.User;
 import com.auth0.net.TokenRequest;
 import edu.BellevueCollege.NestedCatjam.ControlCognizant.config.ApplicationProperties;
+import jdk.jshell.spi.ExecutionControl;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -75,5 +82,21 @@ public class UserManagementService {
 
     public void assignRole(String id, List<String> role) throws Auth0Exception {
         getApi().users().addRoles(id, role).execute();
+    }
+
+    public List<Member> getMembers(Authentication authentication) throws Auth0Exception{
+        final var api = getApi();
+        final var organizations = api.users().getOrganizations(authentication.getName(), new PageFilter()).execute().getItems();
+        return organizations.stream().flatMap(organization -> {
+            try {
+                return api.organizations().getMembers(organization.getId(), new PageFilter()).execute().getItems().stream();
+            } catch (Auth0Exception e) {
+                throw new RuntimeException(e);
+            }
+        }).toList();
+    }
+
+    public void removeMember(String organizationID, String userID) throws Auth0Exception {
+        getApi().organizations().deleteMembers(organizationID, new Members(List.of(userID))).execute();
     }
 }
