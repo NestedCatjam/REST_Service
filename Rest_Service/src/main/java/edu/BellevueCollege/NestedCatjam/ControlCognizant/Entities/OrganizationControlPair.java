3894c7bbb4c6912ec2c2f8ef6575c7734597032e
Saagar <Saagar.Ahluwalia@bellevuecollege.edu>
2023-05-18 17:45:16 -0700

    Create OrganizationControlPair.java

---
+++ Rest_Service/src/main/java/edu/BellevueCollege/NestedCatjam/ControlCognizant/Entities/OrganizationControlPair.java
@@ -0,0 +1,17 @@
+//package edu.BellevueCollege.NestedCatjam.ControlCognizant.Entities;
+//
+//import lombok.Data;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//
+//@Entity
+//@Data
+//public class OrganizationControlPair {
+//    @ManyToOne
+//    @JoinColumn(name = "control_control_id")
+//    private NistControl control;
+//    private String organizationID;
+//
+//}
